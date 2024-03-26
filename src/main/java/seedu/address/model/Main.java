2288f8a0efe6da2a1afff097cf1bc26298e7fb5e
Jerome-j <jerome@outlook.sg>
2024-03-26 09:56:03 +0800

    Add PhonePredicate test

---
+++ src/main/java/seedu/address/model/Main.java
@@ -0,0 +1,14 @@
+//package seedu.address.model;
+//
+//import seedu.address.model.person.Phone;
+//
+//public class Main {
+//    public static void main(String[] args) {
+//        try {
+//            Phone phone = new Phone("123");
+//            System.out.println(phone);
+//        } catch (Exception e) {
+//            System.out.println(e);
+//        }
+//    }
+//}
